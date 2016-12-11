c9b4bc5e158aecaacfbade5107b5dc8216e3edf4
Julien BONACHERA <julien.bonachera@smile.fr>
2016-12-11 13:34:36 +0100

    add dns-01 support

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ MAINTAINER Julien BONACHERA <julien@bonachera.fr>
 RUN pacman -S --noconfirm openssl
 ADD https://raw.githubusercontent.com/lukas2511/dehydrated/master/dehydrated  /usr/local/bin/dehydrated
 COPY letsencrypt_loop  /usr/local/bin/letsencrypt_loop
-RUN chmod +x /usr/local/bin/letsencrypt_loop /usr/local/bin/dehydrated
+COPY hook-* /usr/local/bin/
+RUN chmod +x /usr/local/bin/letsencrypt_loop /usr/local/bin/dehydrated /usr/local/bin/hook-*
 RUN mkdir /etc/letsencrypt
 CMD /usr/local/bin/letsencrypt_loop
---
+++ hook-nsupdate.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+#
+# Example how to deploy a DNS challenge using nsupdate
+#
+
+set -e
+set -u
+set -o pipefail
+
+NSUPDATE="nsupdate"
+# Env vars
+#DNSSERVER
+#ZONE=$ZONE
+#KEY_ALG
+#KEY_NAME
+#KEY
+TTL=0
+
+case "$1" in
+    "deploy_challenge")
+        printf "key $KEY_ALG:$KEY_NAME $KEY\nserver %s\nzone %s.\nupdate add _acme-challenge.%s. %d in TXT \"%s\"\nsend\n" "${DNSSERVER}" "${ZONE}" "${2}" "${TTL}" "${4}" | $NSUPDATE
+        ;;
+    "clean_challenge")
+        printf "key $KEY_ALG:$KEY_NAME $KEY\nserver %s\nzone %s.\nupdate delete _acme-challenge.%s. %d in TXT \"%s\"\nsend\n" "${DNSSERVER}" "${ZONE}" "${2}" "${TTL}" "${4}" | $NSUPDATE
+        ;;
+    "deploy_cert")
+        # do nothing for now
+        ;;
+    "unchanged_cert")
+        # do nothing for now
+        ;;
+    *)
+        echo Unknown hook "${1}"
+        exit 1
+        ;;
+esac
+
+exit 0
